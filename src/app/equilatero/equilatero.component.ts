4392e239cebcbab4aaeea2becdc984d561b739a9
vivimardones <des.viviana.mardones@gmail.com>
2024-12-22 03:09:15 -0300

    Se crea formulario del Equilatero

--- src/app/equilatero/equilatero.component.ts
+++ src/app/equilatero/equilatero.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IonList,
+  IonItem,
+  IonInput,
+  IonButton,
+} from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { TrianguloEquilatero } from '../modelo/FiguraGeometrica';
 
 @Component({
   selector: 'app-equilatero',
+  standalone: true,
   templateUrl: './equilatero.component.html',
   styleUrls: ['./equilatero.component.scss'],
-  standalone: true,
+  imports: [FormsModule, IonList, IonItem, IonInput, IonButton],
 })
-export class EquilateroComponent  implements OnInit {
-
-  constructor() { }
+export class EquilateroComponent implements OnInit {
+  calcularPerimetroStr: string = '';
+  resultado: string = '';
+  constructor() {}
 
   ngOnInit() {}
-
+  calcularPerimetro() {
+    const lado = parseInt(this.calcularPerimetroStr);
+    const circulo = new TrianguloEquilatero(lado);
+    const perimetro = circulo.calcularPerimetro();
+    this.resultado = `El Perimetro es: ${perimetro}`;
+  }
 }
