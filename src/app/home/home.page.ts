bf900b47a305df90e82d45db77da57149226f4f2
vivimardones <des.viviana.mardones@gmail.com>
2024-12-22 02:33:12 -0300

    Se modifica el Home con nuevas componentes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CirculoComponent } from '../circulo/circulo.component';
+import { EquilateroComponent } from '../equilatero/equilatero.component';
+import { EscalenoComponent } from '../escaleno/escaleno.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { IonSelectCustomEvent, SelectChangeEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    CirculoComponent,
+    EquilateroComponent,
+    EscalenoComponent,
+    IonSelect, 
+    IonSelectOption],
 })
 export class HomePage {
+  figura: string = "";
+
   constructor() {}
+
+  esCirculo() { return this.figura === 'circulo'; }
+  esEquilatero() { return this.figura === 'equilatero'; }
+  esEscaleno() { return this.figura === 'escaleno'; }
+
+  selecioneFigura($event: IonSelectCustomEvent<SelectChangeEventDetail<any>>) {
+    console.log('Selecionou a figura');
+  }
 }
